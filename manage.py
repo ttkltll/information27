bd6978526227a4244b12151f88f1bc3aa97b0fef
ttkltll <ttkltll@163.com>
2019-03-11 12:34:17 +0800

    第6次修改,加flask_script,session

--- manage.py
+++ manage.py
@@ -1,13 +1,18 @@
 
 from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
-from flask import Flask
+from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
+# 可以用来指定 session 保存的位置
+from flask_session import Session
+from flask_script import Manager
 
 
 class Config(object):
     """项目的配置"""
     DEBUG = True
+
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
 
     # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information27"
@@ -16,6 +21,17 @@ class Config(object):
     # Redis的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -28,12 +44,17 @@ db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启当前项目 CSRF 保护，只做服务器验证功能
 CSRFProtect(app)
+# 设置session保存指定位置
+Session(app)
+
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
+    session["name"] = "itheima"
     return 'index33333'
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
