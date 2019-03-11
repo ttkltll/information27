05a759a7e9a85316a70ddb8e0fd1d32d662944dc
ttkltll <ttkltll@163.com>
2019-03-11 12:37:57 +0800

    第7次修改,加migrate

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # 可以用来指定 session 保存的位置
 from flask_session import Session
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -48,6 +49,10 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+# 将 app 与 db 关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
