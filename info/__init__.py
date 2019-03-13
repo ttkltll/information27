4289c42f61b306eee0381059e0714a0d76e1ec0d
ttkltll <ttkltll@163.com>
2019-03-13 20:44:49 +0800

    Revert "第14次修改,蓝图2"
    This reverts commit 2288ec79

--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,5 @@
+
+
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -9,14 +11,11 @@ from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
 
 from config import config
+from info.modules.index import index_blu
 
 # 初始化数据库
 #  在Flask很多扩展里面都可以先初始化扩展的对象，然后再去调用 init_app 方法去初始化
 db = SQLAlchemy()
-
-# https://www.cnblogs.com/xieqiankun/p/type_hints_in_python3.html
-redis_store = None  # type: StrictRedis
-# redis_store: StrictRedis = None
 
 
 def setup_log(config_name):
@@ -42,15 +41,14 @@ def create_app(config_name):
     # 通过app初始化
     db.init_app(app)
     # 初始化 redis 存储对象
-    global redis_store
     redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
     # 开启当前项目 CSRF 保护，只做服务器验证功能
     CSRFProtect(app)
     # 设置session保存指定位置
     Session(app)
 
+
     # 注册蓝图
-    from info.modules.index import index_blu
     app.register_blueprint(index_blu)
 
     return app
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,6 @@
-from info import redis_store
 from . import index_blu
 
 
 @index_blu.route('/')
 def index():
-    # 向redis中保存一个值 name itcast
-    redis_store.set("name", "itcast")
     return 'index'
