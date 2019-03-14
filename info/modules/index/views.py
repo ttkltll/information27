e80c59ecd6cc2ff4610a5254c9937cb0942ce7f8
ttkltll <ttkltll@163.com>
2019-03-14 11:48:31 +0800

    标记 This reverts commit 4289c42f

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from info import redis_store
 from . import index_blu
 from flask import  render_template
@@ -6,3 +7,7 @@ from flask import  render_template
 def index():
     # 向redis中保存一个值 name itcast
     return render_template('news/index.html')
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
