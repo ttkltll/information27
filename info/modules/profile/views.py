b4f99165725c0457391668341f5a6083f4b4a934
ttkltll <ttkltll@163.com>
2019-04-18 10:49:09 +0800

    修改用户密码

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -11,6 +11,31 @@ from info.utils.image_storage import storage
 from info.utils.response_code import RET
 
 
+@profile_blu.route('/pass_info', methods=["GET", "POST"])
+@user_login_data
+def pass_info():
+    if request.method == "GET":
+        return render_template('news/user_pass_info.html')
+
+    # 1. 获取参数
+    old_password = request.json.get("old_password")
+    news_password = request.json.get("new_password")
+
+    # 2. 校验参数
+    if not all([old_password, news_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 判断旧密码是否正确
+    user = g.user
+    if not user.check_password(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码错误")
+
+    # 4. 设置新密码
+    user.password = news_password
+
+    return jsonify(errno=RET.OK, errmsg="保存成功")
+
+
 @profile_blu.route('/pic_info', methods=["GET", "POST"])
 @user_login_data
 def pic_info():
@@ -36,7 +61,7 @@ def pic_info():
 
     # 3. 保存头像地址
     user.avatar_url = key
-    return jsonify(errno=RET.OK, errmsg="OK", data={"avatar_url":constants.QINIU_DOMIN_PREFIX+key})
+    return jsonify(errno=RET.OK, errmsg="OK", data={"avatar_url": constants.QINIU_DOMIN_PREFIX + key})
 
 
 @profile_blu.route('/base_info', methods=["GET", "POST"])
