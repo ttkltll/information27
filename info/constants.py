6bc07a9dd994e7a4425f1a0269efcd4e2bd17d8f
ttkltll <ttkltll@163.com>
2019-04-23 15:10:03 +0800

    用户收藏的前后端

--- info/constants.py
+++ info/constants.py
@@ -13,7 +13,7 @@ HOME_PAGE_MAX_NEWS = 10
 # 用户的关注每一页最多数量
 USER_FOLLOWED_MAX_COUNT = 4
 
-# 用户收藏最多新闻数量
+# 用户收藏显示最多新闻数量
 USER_COLLECTION_MAX_NEWS = 10
 
 # 其他用户每一页最多新闻数量
@@ -27,5 +27,4 @@ ADMIN_USER_PAGE_MAX_COUNT = 10
 
 # 管理员页面新闻每页多最数据条数
 ADMIN_NEWS_PAGE_MAX_COUNT = 10
-
 
