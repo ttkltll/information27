fd1596b65645534d1c71047e09e2076759b03ac9
ttkltll <ttkltll@163.com>
2019-04-24 10:42:07 +0800

    新闻发布页面：界面

--- info/static/news/js/user_pass_info.js
+++ info/static/news/js/user_pass_info.js
@@ -17,7 +17,7 @@ $(function () {
         });
         // 取到两次密码进行判断
         var new_password = params["new_password"];
-        var new_password2 = params["new_password2];
+        var new_password2 = params["new_password2"];
 
         if (new_password != new_password2) {
             alert('两次密码输入不一致')
