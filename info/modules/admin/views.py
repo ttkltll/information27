d73b21eb7854cab198f36f92c5eb1d85bc73d15a
ttkltll <ttkltll@163.com>
2019-04-25 14:38:12 +0800

    后台用户列表展示

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -9,18 +9,54 @@ from flask import request
 from flask import session
 from flask import url_for
 
+from info import constants
 from info.models import User
 from info.modules.admin import admin_blu
 from info.utils.common import user_login_data
 
 
+@admin_blu.route('/user_list')
+def user_list():
+    page = request.args.get("page", 1)
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    users = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = User.query.filter(User.is_admin == False).paginate(page, constants.ADMIN_USER_PAGE_MAX_COUNT)
+        users = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 进行模型列表转字典列表
+    user_dict_li = []
+    for user in users:
+        user_dict_li.append(user.to_admin_dict())
+
+    data = {
+        "users": user_dict_li,
+        "total_page": total_page,
+        "current_page": current_page,
+    }
+
+    return render_template('admin/user_list.html', data=data)
+
+
 @admin_blu.route('/user_count')
 def user_count():
-
     # 总人数
     total_count = 0
     try:
-        total_count = User.query.filter(User.is_admin==False).count()
+        total_count = User.query.filter(User.is_admin == False).count()
     except Exception as e:
         current_app.logger.error(e)
 
@@ -29,7 +65,7 @@ def user_count():
     t = time.localtime()
     begin_mon_date = datetime.strptime(('%d-%02d-01' % (t.tm_year, t.tm_mon)), "%Y-%m-%d")
     try:
-        mon_count = User.query.filter(User.is_admin==False, User.create_time > begin_mon_date).count()
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > begin_mon_date).count()
     except Exception as e:
         current_app.logger.error(e)
 
@@ -37,10 +73,9 @@ def user_count():
     day_count = 0
     begin_day_date = datetime.strptime(('%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)), "%Y-%m-%d")
     try:
-        day_count = User.query.filter(User.is_admin==False, User.create_time > begin_day_date).count()
+        day_count = User.query.filter(User.is_admin == False, User.create_time > begin_day_date).count()
     except Exception as e:
         current_app.logger.error(e)
-
 
     # 拆线图数据
 
@@ -57,7 +92,8 @@ def user_count():
         begin_date = today_date - timedelta(days=i)
         # 取到下一天的0点0分
         end_date = today_date - timedelta(days=(i - 1))
-        count = User.query.filter(User.is_admin == False, User.last_login >= begin_date, User.last_login < end_date).count()
+        count = User.query.filter(User.is_admin == False, User.last_login >= begin_date,
+                                  User.last_login < end_date).count()
         active_count.append(count)
         active_time.append(begin_date.strftime('%Y-%m-%d'))
 
@@ -90,7 +126,7 @@ def login():
     if request.method == "GET":
         # 判断当前是否有登录，如果有登录直接重定向到管理员后台主页
         user_id = session.get("user_id", None)
-        is_admin = session.get("is_admin",False)
+        is_admin = session.get("is_admin", False)
         if user_id and is_admin:
             return redirect(url_for('admin.index'))
 
@@ -106,7 +142,7 @@ def login():
 
     # 查询当前用户
     try:
-        user = User.query.filter(User.mobile==username, User.is_admin==True).first()
+        user = User.query.filter(User.mobile == username, User.is_admin == True).first()
     except Exception as e:
         current_app.logger.error(e)
         return render_template('admin/login.html', errmsg="用户信息查询失败")
