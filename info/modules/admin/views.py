d7c101e6f18e90de37c03f035af127b89d624d4f
ttkltll <ttkltll@163.com>
2019-04-25 13:51:05 +0800

    添加测试用户

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from flask import g
 from flask import redirect
 from flask import render_template
 from flask import request
@@ -7,14 +8,14 @@ from flask import url_for
 
 from info.models import User
 from info.modules.admin import admin_blu
-
-
-
+from info.utils.common import user_login_data
 
 
 @admin_blu.route('/index')
+@user_login_data
 def index():
-    return render_template('admin/index.html')
+    user = g.user
+    return render_template('admin/index.html', user=user.to_dict())
 
 
 @admin_blu.route('/login', methods=["GET", "POST"])
